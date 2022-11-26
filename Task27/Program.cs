ce4bc630c576c65c60a7cee3a5b83e5598a721e4
Marina <sizemskaya@gmail.com>
2022-11-26 19:18:23 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+using System;
+using static System.Console;
+Clear();
+Write("Введите число А: ");
+
+string str = Console.ReadLine();
+int summa = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    int currentNumber = int.Parse(str[i].ToString());
+    summa+=currentNumber;
+}
+Console.WriteLine($"Сумма цифр в числе {str} равна {summa}");
